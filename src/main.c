a7cc268aa26ad1eee5bec9cf123c42eb0d42a556
Eliott Belinguier <eliott@belinguier.fr>
2024-11-25 10:24:42 +0000

    Refactor: Update structure names and associated function calls
    - Renamed structure `t_map` to `map_s` for improved consistency.
    - Updated related function calls from `createMapFromFile` to `map_from_file`.
    - Renamed fields `x_max` and `y_max` to `width` and `height` respectively for better clarity.
    - Refactored map display logic to use `map_display`.

--- src/main.c
+++ src/main.c
@@ -2,35 +2,35 @@
 #include "map.h"
 
 int main() {
-    t_map map;
+    map_s map;
 
     // The following preprocessor directive checks if the code is being compiled on a Windows system.
     // If either _WIN32 or _WIN64 is defined, it means we are on a Windows platform.
     // On Windows, file paths use backslashes (\), hence we use the appropriate file path for Windows.
 #if defined(_WIN32) || defined(_WIN64)
-    map = createMapFromFile("..\\maps\\example1.map");
+    map = map_from_file("..\\maps\\example1.map");
 #else
-    map = createMapFromFile("../maps/example1.map");
+    map = map_from_file("../maps/example1.map");
 #endif
 
-    printf("Map created with dimensions %d x %d\n", map.y_max, map.x_max);
-    for (int i = 0; i < map.y_max; i++)
+    printf("Map created with dimensions %d x %d\n", map.height, map.width);
+    for (int i = 0; i < map.height; i++)
     {
-        for (int j = 0; j < map.x_max; j++)
+        for (int j = 0; j < map.width; j++)
         {
             printf("%d ", map.soils[i][j]);
         }
         printf("\n");
     }
     // printf the costs, aligned left 5 digits
-    for (int i = 0; i < map.y_max; i++)
+    for (int i = 0; i < map.height; i++)
     {
-        for (int j = 0; j < map.x_max; j++)
+        for (int j = 0; j < map.width; j++)
         {
             printf("%-5d ", map.costs[i][j]);
         }
         printf("\n");
     }
-    displayMap(map);
+    map_display(map);
     return 0;
 }
